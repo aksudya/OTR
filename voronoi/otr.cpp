62c2e358645c523b2c410efa213f0967d43f78a9
aksudya <467498869@qq.com>
2019-10-30 09:50:54 +0800

    change

--- voronoi/otr.cpp
+++ voronoi/otr.cpp
@@ -10,3 +10,13 @@ void OTR::Init(vector<Point> input)
 	delaunay_input.insert(input.begin(), input.end());
 	
 }
+
+void OTR::InitPriQueue()
+{
+	for (auto eiter=delaunay_input.finite_edges_begin();eiter!=delaunay_input.finite_edges_begin();eiter++)
+	{
+		Segment s = delaunay_input.segment(eiter);
+		Point a = s.point(0);
+		Point b = s.point(1);
+	}
+}
--- voronoi/otr.h
+++ voronoi/otr.h
@@ -2,7 +2,7 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/point_generators_2.h>
 #include <CGAL/Optimal_transportation_reconstruction_2.h>
-#include <GL/GLUT.h>
+#include <GLUT.h>
 #include <iostream>
 #include <cmath>
 #include <random>
@@ -13,8 +13,9 @@ using namespace std;
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef K::Point_2                                          Point;
 typedef CGAL::Delaunay_triangulation_2<K>					Delaunay;
-typedef  Delaunay::Edge										Half_edge;
-typedef  Delaunay::Vertex_handle							vertex_handle;
+typedef Delaunay::Edge										Half_edge;
+typedef Delaunay::Vertex_handle							vertex_handle;
+typedef K::Segment_2 Segment;
 
 class pri_queue_item
 {
@@ -35,11 +36,15 @@ public:
 	vector<Point> points_input;
 	Delaunay delaunay_input;
 
-	priority_queue<pri_queue_item> half_edge_queue;
+	Delaunay delaunay_temp;
 
-	map<vertex_handle, vector<Point>> vertex_points_map;
-	map<Half_edge, vector<Point>> edge_points_map;
+	priority_queue<pri_queue_item> half_edge_queue;					//优先队列
 
+	map<vertex_handle, vector<Point>> vertex_points_map;			//点分配到边
+	map<Half_edge, vector<Point>> edge_points_map;					//点分配到顶点
+
+	map<vertex_handle, vector<Point>> vertex_points_map_temp;			//点分配到边	 临时
+	map<Half_edge, vector<Point>> edge_points_map_temp;					//点分配到顶点	临时
 
 
 	OTR();
@@ -47,6 +52,6 @@ public:
 
 	void Init(vector<Point> input);
 
-
+	void InitPriQueue();
 
 };
